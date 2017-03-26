bafa57dd64b0d666c3d795329f3c6488b46f655a
Erika Vidal <erikadianavidalcapillo@gmail.com>
2017-03-26 18:08:23 -0500

    arreglando alerts

--- js/main.js
+++ js/main.js
@@ -1,48 +1,62 @@
+
 function validateForm(){
 	/* Escribe tú código aquí */
-  var formularios = document.getElementsByTagName("input");
-//validar todos los campos
+  var nombre = document.getElementById("name");
+  var apellido = document.getElementById("lastname");
+  var email = document.getElementById("input-email");
+  var password = document.getElementById("input-password");
+  var indice = document.getElementById("opcion");
+//validar los 4 campos
 
-  for(var i = 0; i < 4; i++){
-     if(formularios[i].value==""){
-       alert("Los 4 primeros campos son obligatorios");
-       formularios[i].focus();
+     if(nombre.value==""){
+       alert("El Nombre es obligatorio");
+       nombre.focus();
        return false;
-     }
-  }
+     }else if (apellido.value=="") {
+       alert("El Apellido es obligatorio");
+       apellido.focus();
+       return false;
+     }else if(email.value==""){
+       alert("El email es obligatorio");
+       email.focus();
+       return false;
+    }else if(password.value==""){
+      alert("La contraseña es obligatoria");
+      password.focus();
+      return false;
+    }else if(indice.value==0){
+       alert("Seleccione un tipo de bicicleta");
+       return false;
+    }
   var patron=/^([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]+[\s]*)+$/;
-      if(!patron.test(formularios[0].value)){ //validar nombre
+      if(!patron.test(nombre.value)){ //validar nombre
         alert("Escriba la primera letra de su nombre con Mayúscula");
-        formularios[0].focus();
+        nombre.focus();
         return false;
       }
-      if(!patron.test(formularios[1].value)){//validar apellido
+      if(!patron.test(apellido.value)){//validar apellido
         alert("Escriba la primera letra de su apellido con Mayúscula");
-        formularios[1].focus();
+        apellido.focus();
         return false;
       }
   var patron2=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
 
-      if(!patron2.test(formularios[2].value)){//validar correo
+      if(!patron2.test(email.value)){//validar correo
         alert("Ingrese un correo válido\nEjemplo : yo@ejemplo.com");
-        formularios[2].focus();
+        email.focus();
         return false;
       }
           //validar password
-      if(formularios[3].value.length<6){
+      if(password.value.length<6){
         alert("Su contraseña debe tener al menos 6 caracteres");
-        formularios[3].focus();
+        password.focus();
         return false;
-      }else if (formularios[3].value=="password"||formularios[3].value=="123456"|| formularios[3].value=="098754") {
-        alert("Su contraseña no puede ser " + formularios[3].value);
-        formularios[3].focus();
+      }else if (password.value=="password"||password.value=="123456"|| password.value=="098754") {
+        alert("Su contraseña no puede ser " + password.value);
+        password.focus();
         return false;
       }
 
-  var indice = document.getElementById("opcion");
-      if(indice.value==0){
-        alert("Seleccione un tipo de bicicleta");
-        return false;
-      }
+
 
 }
